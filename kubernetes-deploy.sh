43c48f0dd9913f3ec3f9eaba0eb5b516a7462e85
Jiayin Zhuo <jiayin.zhuo@gmail.com>
2019-11-11 21:30:25 -0500

    Remove out of date kubectl command

--- kubernetes-deploy.sh
+++ kubernetes-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-kubectl run apache --image=jiayinzhuo/microservice-kubernetes-demo-apache --port=80
-kubectl expose deployment/apache --type="LoadBalancer" --port 80
-kubectl run catalog --image=jiayinzhuo/catalog --port=8080
-kubectl expose deployment/catalog --type="LoadBalancer" --port 8080
-kubectl run customer --image=jiayinzhuo/customer --port=8080
-kubectl expose deployment/customer --type="LoadBalancer" --port 8080
-kubectl run order --image=jiayinzhuo/order --port=8080
-kubectl expose deployment/order --type="LoadBalancer" --port 8080
-kubectl run hystrix-dashboard --image=jiayinzhuo/hystrix-dashboard --port=8080
-kubectl expose deployment/hystrix-dashboard --type="LoadBalancer" --port 8080
+kubectl create deployment apache --image=jiayinzhuo/microservice-kubernetes-demo-apache
+kubectl expose deployment apache --type="LoadBalancer" --port 80
+kubectl create deployment catalog --image=jiayinzhuo/catalog
+kubectl expose deployment catalog --type="LoadBalancer" --port 8080
+kubectl create deployment customer --image=jiayinzhuo/customer
+kubectl expose deployment customer --type="LoadBalancer" --port 8080
+kubectl create deployment order --image=jiayinzhuo/order
+kubectl expose deployment order --type="LoadBalancer" --port 8080
+kubectl create deployment hystrix-dashboard --image=jiayinzhuo/hystrix-dashboard
+kubectl expose deployment hystrix-dashboard --type="LoadBalancer" --port 8080
